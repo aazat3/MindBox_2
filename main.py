92452638d64760d60b1bc19da4e568cb13f645b1
aazat3 <atavlykaev@list.ru>
2024-04-09 12:42:22 +0500

    Ver 2.

--- main.py
+++ main.py
@@ -14,8 +14,13 @@ class ShapeCalculator:
     def triangle_area(side1, side2, side3):
         if side1 <= 0 or side2 <= 0 or side3 <= 0:
             raise ValueError("Длины не могут быть отрицательными")
-        s = (side1 + side2 + side3) / 2
-        area = math.sqrt(s * (s - side1) * (s - side2) * (s - side3))
+        if ShapeCalculator.is_right_triangle(side1, side2, side3):
+            sides = [side1, side2, side3]
+            sides.remove(max(sides))
+            area = sides[0] * sides[1] / 2
+        else:
+            s = (side1 + side2 + side3) / 2
+            area = math.sqrt(s * (s - side1) * (s - side2) * (s - side3))
         return area
 
     @staticmethod
@@ -23,8 +28,7 @@ class ShapeCalculator:
         sides = [side1, side2, side3]
         max_side = max(sides)
         sides.remove(max_side)
-        area = math.isclose(max_side ** 2, sides[0] ** 2 + sides[1] ** 2, rel_tol=1e-9)
-        return area
+        return math.isclose(max_side ** 2, sides[0] ** 2 + sides[1] ** 2, rel_tol=1e-9)
 
     @staticmethod
     def universal_area(*sides):
@@ -39,17 +43,16 @@ class ShapeCalculator:
 
 
 class TestShapeCalculator(unittest.TestCase):
-    def test_circle_area(self):
+    def test_circle_area(self): # Площадь круга
         self.assertAlmostEqual(ShapeCalculator.circle_area(5), 78.54, places=2)
 
-    def test_triangle_area(self):
+    def test_triangle_area(self): # Площадь треугольника
         self.assertAlmostEqual(ShapeCalculator.triangle_area(3, 4, 5), 6)
 
-    def test_is_right_triangle(self):
-        self.assertTrue(ShapeCalculator.is_right_triangle(3, 4, 5))
-        self.assertFalse(ShapeCalculator.is_right_triangle(3, 4, 6))
+    def test_is_right_triangle(self): # Площадь прямоугольного треугольника
+        self.assertAlmostEqual(ShapeCalculator.triangle_area(3, 4, 5), 6)
 
-    def test_universal_area(self):
+    def test_universal_area(self): # Площадь неизвестной фигуры
         self.assertAlmostEqual(ShapeCalculator.universal_area(5), 78.54, places=2)
         self.assertAlmostEqual(ShapeCalculator.universal_area(3, 4, 5), 6)
 
